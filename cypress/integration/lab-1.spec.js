bfbe13e4b692b5648c8e9e8bab2a64fe58f0cc63
Grigorii K. Shartsev <me@shgk.me>
2020-09-12 19:15:27 +0500

    Improve tests and criterias
    Add test for 90%, make tests descriptions same as criterias

--- cypress/integration/lab-1.spec.js
+++ cypress/integration/lab-1.spec.js
@@ -1,24 +1,35 @@
 const baseURL = "./solution/index.html";
 
-const config = {
-  failureThreshold: 1 / 100, // %
-  customDiffConfig: { threshold: 0.1 },
+const matchPercentConfig = (percent) => ({
+  failureThreshold: percent / 100, // %
+  customDiffConfig: { threshold: 0.01 },
   failureThresholdType: "percent",
   allowSizeMismatch: true
-};
+});
 
 describe("Lab-1", () => {
-  it("должны быть ссылки <a href=\"#\">: Письма, Написать, @username, Выход, Непрочитанные, Входящие, Отправленные", () => {
+  it("(3б) Страница в целом соответствует эскизу на 1280x720 с точностью более 90%", () => {
+    cy.viewport(1280, 720);
     cy.visit(baseURL);
-    const links = ["Письма", "Написать", "@username", "Выход", "Непрочитанные", "Входящие", "Отправленные"];
-    cy.get("a").each(($a, index) => {
-      cy.wrap($a)
-        .should("have.text", links[index])
-        .should("have.attr", "href", "#");
-    });
+    cy.screenshot("Lab-1-1280x720-simple-screenshot");
+    cy.matchImageSnapshot("Lab-1-1280x720", matchPercentConfig(10));
   });
 
-  it("должны быть семантические теги: <nav>, <main>, <footer>, <article>, <ul>, <h1>", () => {
+  it("(3б) Страница соответствует эскизу на 1280x720 с точностью более 99.9%", () => {
+    cy.viewport(1280, 720);
+    cy.visit(baseURL);
+    cy.screenshot("Lab-1-1280x720-screenshot");
+    cy.matchImageSnapshot("Lab-1-1280x720", matchPercentConfig(0.1));
+  });
+
+  it("(2б) Страница соответствует эскизу на 1024x576 с точностью более 99.9%", () => {
+    cy.viewport(1024, 576);
+    cy.visit(baseURL);
+    cy.screenshot("Lab-1-1024x576-screenshot");
+    cy.matchImageSnapshot("Lab-1-1024x576", matchPercentConfig(0.1));
+  });
+
+  it("(1б) Использованы семантические теги (nav, main, footer, article, ul, h1, time)\n присутствуют ссылки <a href=\"#\">: Письма, Написать, @username, Выход, Непрочитанные, Входящие, Отправленные", () => {
     cy.visit(baseURL);
     cy.get("nav");
     cy.get("main");
@@ -26,10 +37,7 @@ describe("Lab-1", () => {
     cy.get("article");
     cy.get("ul");
     cy.get("h1");
-  });
 
-  it("должны быть элементы <time> для дат", () => {
-    cy.visit(baseURL);
     const dates = [
       ["2020-08-28", "28.08.2020"],
       ["2020-08-28", "28.08.2020"],
@@ -40,20 +48,13 @@ describe("Lab-1", () => {
         .should("have.attr", "datetime", dates[index][0])
         .should("have.text", dates[index][1]);
     });
-  });
 
-  it("должно соответствовать эталону с точностью > 99% на 1280x720", () => {
-    cy.viewport(1280, 720);
-    cy.visit(baseURL);
-    cy.screenshot("Lab-1-1280x720-screenshot");
-    cy.matchImageSnapshot("Lab-1-1280x720", config);
-  });
-
-  it("должно соответствовать эталону с точностью > 99% на 1024x576", () => {
-    cy.viewport(1024, 576);
-    cy.visit(baseURL);
-    cy.screenshot("Lab-1-1024x576-screenshot");
-    cy.matchImageSnapshot("Lab-1-1024x576", config);
+    const links = ["Письма", "Написать", "@username", "Выход", "Непрочитанные", "Входящие", "Отправленные"];
+    cy.get("a").each(($a, index) => {
+      cy.wrap($a)
+        .should("have.text", links[index])
+        .should("have.attr", "href", "#");
+    });
   });
 });
 
