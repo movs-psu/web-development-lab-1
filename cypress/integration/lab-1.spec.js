34daa391b60566e767ea87609f61564f75a2df52
Grigorii K. Shartsev <me@shgk.me>
2020-09-12 12:10:21 +0500

    Add tests for semantics

--- cypress/integration/lab-1.spec.js
+++ cypress/integration/lab-1.spec.js
@@ -8,17 +8,52 @@ const config = {
 };
 
 describe("Lab-1", () => {
+  it("should have links <a href=\"#\"> Письма, Написать, @username, Выход, Непрочитанные, Входящие, Отправленные", () => {
+    cy.visit(baseURL);
+    const links = ["Письма", "Написать", "@username", "Выход", "Непрочитанные", "Входящие", "Отправленные"];
+    cy.get("a").each(($a, index) => {
+      cy.wrap($a)
+        .should("have.text", links[index])
+        .should("have.attr", "href", "#");
+    });
+  });
+
+  it("should have semantic elements: <nav>, <main>, <footer>, <article>, <ul>, <h1>", () => {
+    cy.visit(baseURL);
+    cy.get("nav");
+    cy.get("main");
+    cy.get("footer");
+    cy.get("article");
+    cy.get("ul");
+    cy.get("h1");
+  });
+
+  it("should have <time> elements", () => {
+    cy.visit(baseURL);
+    const dates = [
+      ["2020-08-28", "28.08.2020"],
+      ["2020-08-28", "28.08.2020"],
+      ["2020-08-26", "26.08.2020"]
+    ];
+    cy.get("time").each(($time, index) => {
+      cy.wrap($time)
+        .should("have.attr", "datetime", dates[index][0])
+        .should("have.text", dates[index][1]);
+    });
+  });
+
   it("match screenshot on 1280x720", () => {
     cy.viewport(1280, 720);
     cy.visit(baseURL);
-    cy.screenshot('Lab-1-1280x720-screenshot');
-    cy.matchImageSnapshot('Lab-1-1280x720', config);
+    cy.screenshot("Lab-1-1280x720-screenshot");
+    cy.matchImageSnapshot("Lab-1-1280x720", config);
   });
 
   it("match screenshot on 1024x576", () => {
     cy.viewport(1024, 576);
     cy.visit(baseURL);
-    cy.screenshot('Lab-1-1024x576-screenshot');
-    cy.matchImageSnapshot('Lab-1-1024x576', config);
+    cy.screenshot("Lab-1-1024x576-screenshot");
+    cy.matchImageSnapshot("Lab-1-1024x576", config);
   });
 });
+
