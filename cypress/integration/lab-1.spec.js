07d5c46df89b9b7c2f2339b33dc1ef4de3bdbe3a
Grigorii K. Shartsev <me@shgk.me>
2020-09-14 11:56:03 +0500

    Update lab design for best testing and easy solution

--- cypress/integration/lab-1.spec.js
+++ cypress/integration/lab-1.spec.js
@@ -2,34 +2,48 @@ const baseURL = "./solution/index.html";
 
 const matchPercentConfig = (percent) => ({
   failureThreshold: percent / 100, // %
-  customDiffConfig: { threshold: 0.06 },
+  customDiffConfig: { threshold: 0.5 },
   failureThresholdType: "percent",
   allowSizeMismatch: true
 });
 
+// > 10% - всё очень плохо, нет одного сообщения, или нет меню
+// 10% --- В целом зачтено
+// 8.4% - нет аватарок
+// 5% - куча косяков с размерами
+// 1.28..1.6% - мелкая проблема с колонками, например 87% вместо 1/6
+// 0.48% - отсутствует активная ссылка в главном меню
+// 0.18..0.26% - неверная колонка только в навбаре
+// 0.08% --- Зачтено полностью
+// 0.05..0.07% - Arial вместо Open Sans + тень 2px вокруг всего текста (на самом деле поехала из-за смены шрифта кнопка)
+// 0.038% - невидимые аватарки
+// 0.02% --- Зачтено полностью
+// 0.01% - запас
+// 0.0066..0.0094% - адская тень у шрифта, всё ужасно двоится
+
 describe("Lab-1", () => {
-  it("(3б) Страница в целом соответствует эскизу на 1280x720 с точностью более 97%", () => {
+  it("(3б) Страница в целом соответствует эскизу на 1280x720 с точностью более 90%", () => {
     cy.viewport(1280, 720);
     cy.visit(baseURL);
     cy.screenshot("Lab-1-1280x720-simple-screenshot");
-    cy.matchImageSnapshot("Lab-1-1280x720", matchPercentConfig(3));
+    cy.matchImageSnapshot("Lab-1-1280x720", matchPercentConfig(10));
   });
 
-  it("(3б) Страница соответствует эскизу на 1280x720 с точностью более 99%", () => {
+  it("(3б) Страница соответствует эскизу на 1280x720 с точностью более 99.98%", () => {
     cy.viewport(1280, 720);
     cy.visit(baseURL);
     cy.screenshot("Lab-1-1280x720-screenshot");
-    cy.matchImageSnapshot("Lab-1-1280x720", matchPercentConfig(1));
+    cy.matchImageSnapshot("Lab-1-1280x720", matchPercentConfig(0.02)); //0.08%
   });
 
-  it("(2б) Страница соответствует эскизу на 1024x576 с точностью более 99%", () => {
+  it("(2б) Страница соответствует эскизу на 1024x576 с точностью более 99.98%", () => {
     cy.viewport(1024, 576);
     cy.visit(baseURL);
     cy.screenshot("Lab-1-1024x576-screenshot");
-    cy.matchImageSnapshot("Lab-1-1024x576", matchPercentConfig(1));
+    cy.matchImageSnapshot("Lab-1-1024x576", matchPercentConfig(0.02));
   });
 
-  it("(1б) Использованы семантические теги (nav, main, footer, article, ul, h1, time)\n присутствуют ссылки <a href=\"#\">: Письма, Написать, @username, Выход, Непрочитанные, Входящие, Отправленные", () => {
+  it("(1б) Использованы семантические теги (nav, main, footer, article, ul, h1, time); присутствуют ссылки <a href=\"#\">: Письма, Написать, @username, Выход, Непрочитанные, Входящие, Отправленные", () => {
     cy.visit(baseURL);
     cy.get("nav");
     cy.get("main");
